8fc75370f4ea9f7664a7ac45f1be9951027e5533
TommyNguyen14 <tommy.nguy14@gmail.com>
2023-10-21 03:22:04 -0400

    changing ui and theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,44 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import { Auth } from './components/Auth';
 import PomodoroTimer from './components/PomodoroTimer';
-import Tasks from './components/Tasks';  // Import the Tasks component
+import Tasks from './components/Tasks';
 import { supabase } from './supabaseClient';
-import { ThemeProvider, createTheme, CssBaseline, Button } from '@mui/material';
+import { ThemeProvider, createTheme, CssBaseline, IconButton, AppBar, Toolbar, Typography, Menu, MenuItem, Dialog, DialogContent } from '@mui/material';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import LightModeIcon from '@mui/icons-material/LightMode';
+
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'; // Icon for authentication
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
+  const [authMenuAnchor, setAuthMenuAnchor] = useState(null); // State to manage the anchor element of the auth menu
+  const [session, setSession] = useState(supabase.auth.getSession());
+  const [guest, setGuest] = useState(false);
+  const [user, setUser] = useState(null);
+  const [authDialogOpen, setAuthDialogOpen] = useState(false); // State to manage the authentication dialog
 
   const theme = createTheme({
     palette: {
       mode: darkMode ? 'dark' : 'light',
+      primary: {
+        main: darkMode ? '#A3A3A3' : '#2B2B2B',
+      },
+      secondary: {
+        main: darkMode ? '#C1C1C1' : '#2D2D2D',
+      },
+      login: {
+        main: darkMode ? '#A3A3A3' : '#A3A3A3',
+      },
+    },
+    typography: {
+      fontFamily: 'Inter, sans-serif',
+      h1: {
+        fontSize: '1rem',
+      },
+      h5: {
+        fontWeight: 'bold',
+      },
+      main: darkMode ? '#FFFFFF' : '#CBD7DF',
     },
   });
 
@@ -19,19 +47,27 @@ function App() {
     setDarkMode(!darkMode);
   };
 
-  const [session, setSession] = useState(supabase.auth.getSession());
-  const [guest, setGuest] = useState(false);
-  const [user, setUser] = useState(null); // Add this state to hold the current user
+  const handleOpenAuthMenu = (event) => {
+    setAuthMenuAnchor(event.currentTarget); // Open the auth menu
+  };
 
+  const handleCloseAuthMenu = () => {
+    setAuthMenuAnchor(null); // Close the auth menu
+  };
+
+  const handleOpenAuthDialog = () => {
+    setAuthDialogOpen(true); // Open the authentication dialog
+  };
+
+  const handleCloseAuthDialog = () => {
+    setAuthDialogOpen(false); // Close the authentication dialog
+  };
 
   useEffect(() => {
-
-
     const { data: authListener } = supabase.auth.onAuthStateChange((event, session) => {
       setSession(session);
-      setUser(session?.user); // Update user state here
-      console.log("APP.JSX Auth Event: ", event, "Current User: ", session?.user);
-
+      setUser(session?.user);
+      console.log('APP.JSX Auth Event:', event, 'Current User:', session?.user);
     });
 
     return () => {
@@ -43,22 +79,46 @@ function App() {
     setGuest(true);
   };
 
- 
-
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-    <div className="App">
-    <Button variant="contained" onClick={handleThemeToggle}>
-          Toggle Theme
-        </Button>
-            <Auth onSkip={handleSkip} guestMode={guest} />
-            
-            {guest || session ? <PomodoroTimer /> : null }
-            {guest || session ? <Tasks user={user} />: null }
-            
-        </div>
-        </ThemeProvider>
+      <div className="App">
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h5" align="center" style={{ flexGrow: 1 }}>
+              Slitherman Pomodoro
+            </Typography>
+
+            <IconButton
+              color="primary"
+              onClick={handleOpenAuthDialog}
+            >
+              <AccountCircleIcon sx={{ color: '#A1A1A1' }}/> {/* Icon for authentication */}
+            </IconButton>
+
+            <IconButton color="primary" onClick={handleThemeToggle}>
+              {darkMode ? (
+                <DarkModeIcon />
+              ) : (
+                <LightModeIcon sx={{ color: '#A1A1A1' }} />
+              )}
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        {guest || session ? <Tasks user={user} /> : null}
+        {guest || session ? <PomodoroTimer /> : null}
+
+        <Dialog
+          open={authDialogOpen}
+          onClose={handleCloseAuthDialog}
+          maxWidth="100%" // Set the maximum width of the dialog
+        >
+          <DialogContent sx={{ padding: '25% 85px' }}>
+            <Auth onSkip={handleSkip} guestMode={guest} darkMode={darkMode} />
+          </DialogContent>
+        </Dialog>
+      </div>
+    </ThemeProvider>
   );
 }
 
