1ff6d66b8ecd0daf2a9e784e3d41d54889db260c
TommyNguyen14 <tommy.nguy14@gmail.com>
2023-10-25 02:06:07 -0400

    Color changes

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Button, TextField, Typography, useTheme } from '@mui/material';
+
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
@@ -17,9 +18,7 @@ export function Task({ task, onDelete, onUpdate, onComplete }) {
     };
 
     const isCompleted = task.completed || false; // Ensure it's false if it's null
-    
-
-    const textColor = theme.palette.background.paper;
+    const isDarkMode = theme.palette.mode === 'dark';
 
     return (
         <div className="task-index">
@@ -29,28 +28,28 @@ export function Task({ task, onDelete, onUpdate, onComplete }) {
                     <div className="edit-mode">
                         <TextField fullWidth value={title} onChange={(e) => setTitle(e.target.value)} label="Title" />
                         <TextField fullWidth value={description} onChange={(e) => setDescription(e.target.value)} label="Description" />
-                        <Button onClick={handleUpdate} style={{ color: textColor }}>
-                        <span style={{ color: textColor }}> Save </span>
+                        <Button onClick={handleUpdate} style={{ color: isDarkMode ? 'white' : 'black' }}>
+                        <span style={{ color: isDarkMode ? 'white' : 'black' }}> Save </span>
                         </Button>
                     </div>
                 ) : (
                     <>
                         <div className="task-content">
-                            <h3 className="task-title" style={{ color: textColor }}>{title}</h3>
-                            <p className="task-description" style={{ color: textColor }}>{description}</p>
+                            <h3 className="task-title" style={{ color: isDarkMode ? 'white' : 'black' }}>{title}</h3>
+                            <p className="task-description" style={{ color: isDarkMode ? 'white' : 'black' }}>{description}</p>
                         </div>
-                        <p style={{ color: textColor }}>Status: {isCompleted ? 'Completed' : 'Incomplete'}</p>
-                        <Button startIcon={<EditIcon />} onClick={() => setIsEditing(true)}  style={{ color: textColor }}>
-                        <span style={{ color: textColor }}>Edit</span>
+                        <p style={{ color: isDarkMode ? 'white' : 'black' }}>Status: {isCompleted ? 'Completed' : 'Incomplete'}</p>
+                        <Button startIcon={<EditIcon />} onClick={() => setIsEditing(true)}  style={{ color: isDarkMode ? 'white' : 'black' }}>
+                        <span style={{ color: isDarkMode ? 'white' : 'black' }}>Edit</span>
                         </Button>
-                        <Button startIcon={<DeleteIcon />} onClick={() => onDelete(task.id)} style={{ color: textColor }}>
-                        <span style={{ color: textColor }}>Delete</span>
+                        <Button startIcon={<DeleteIcon />} onClick={() => onDelete(task.id)} style={{ color: isDarkMode ? 'white' : 'black' }}>
+                        <span style={{ color: isDarkMode ? 'white' : 'black' }}>Delete</span>
                         </Button>
                         {isCompleted ? (
                             <CheckCircleIcon color="success" />
                         ) : (
-                            <Button startIcon={<CheckCircleOutlineIcon />} onClick={() => onComplete(task.id)}  style={{ color: textColor }}>
-                                 <span style={{ color: textColor }}>Mark as Complete</span>
+                            <Button startIcon={<CheckCircleOutlineIcon />} onClick={() => onComplete(task.id)}  style={{ color: isDarkMode ? 'white' : 'black' }}>
+                                 <span style={{ color: isDarkMode ? 'white' : 'black' }}>Mark as Complete</span>
                             </Button>
                         )}
                     </>
